11aa53c9b01343114d6b79dd2bbe805db5cf4388
murilodestefani <murilo.destefani@estudante.ifms.edu.br>
2023-08-09 20:26:55 -0400

    add action on button cards

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+function executar1() {
+  alert(`Ação do Botão 1`);
+}
+
+var botao1 = document.getElementById("botao1");
+botao1.addEventListener("click", executar1);
+
+function executar2() {
+  alert(`Ação do Botão 2`);
+}
+
+var botao2 = document.getElementById("botao2");
+botao2.addEventListener("click", executar2);
+
+function executar3() {
+  alert(`Ação do Botão 3`);
+}
+
+var botao3 = document.getElementById("botao3");
+botao3.addEventListener("click", executar3);
